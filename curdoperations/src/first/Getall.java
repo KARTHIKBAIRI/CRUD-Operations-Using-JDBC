bb8cb9af15d7959e17e778f3751efc049d4b1eb0
Karthik Bairi <133215834+KARTHIKBAIRI@users.noreply.github.com>
2025-03-19 23:29:07 +0530

    Update Getall.java

--- curdoperations/src/first/Getall.java
+++ curdoperations/src/first/Getall.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Getall {
 	public static final String  Driver = "com.mysql.cj.jdbc.Driver";
 	public static final String  username = "root";
-	public static final String  password = "Karthikb@11";
+	public static final String  password = "Karthik";
 	public static final String  url = "jdbc:mysql://localhost:3306/demo";
 	public static Connection conn;
 //	prepare statement
